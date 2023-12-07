b16f1dccdc42cb05405f519b602bfa17cc873fa3
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 12:04:17 +0530

    feat: add form submit handler

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Captcha } from "../components/Captcha";
 import { DropDown } from "../components/DropDown";
 import { InputField } from "../components/InputField";
@@ -5,48 +6,77 @@ import { useUser } from "../context/UserContext";
 import { data } from "../data/StateData";
 
 export const Form = () => {
-  const { user, setUser } = useUser();
+  const navigate = useNavigate();
+  const { user, setUser, errors, setErrors, captcha } = useUser();
   const selectedState = data.find((state) => state.state === user.state);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name === "name") {
-      const nameValue = value.replace(/[^A-Za-z]/g, "");
-      setUser({ ...user, userName: nameValue });
-    }
+    setUser({ ...user, [name]: value });
+    setErrors({ ...errors, [name]: "" });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!/^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/.test(user.name)) {
+      setErrors({ ...errors, name: "Name can only contain alphabets." });
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
+      setErrors({ ...errors, email: "Please provide a valid email." });
+    } else if (!/^[0-9]+$/.test(user.contact)) {
+      setErrors({
+        ...errors,
+        contact: "Contact should only contain numbers.",
+      });
+    } else if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
+      setErrors({ ...errors, captcha: "Please provide correct value." });
+    } else {
+      localStorage.setItem("data", JSON.stringify({ user }));
+      navigate("/home");
+    }
+  };
   return (
     <>
-      <div className="w-[360px] p-[30px] shadow-lg rounded">
+      <div className="w-[360px] p-[30px] shadow-lg rounded xs:w-[320px] xs:p-[0px] xs:px-[15px] xs:py-[20px]">
         <h1 className="font-extrabold text-[36px] text-center text-primary-dark mb-[20px]">
           Apply
         </h1>
-        <form className="flex flex-col">
+        <form className="flex flex-col" onSubmit={handleSubmit}>
           <InputField
             name="name"
             type="text"
-            value={user.userName}
+            value={user.name}
             handleChange={handleChange}
           />
+          <p className="text-red-500 text-[12px] mt-[-5px]">
+            {errors?.name && errors.name}
+          </p>
           <InputField
             name="email"
             type="text"
             value={user.email}
             handleChange={handleChange}
           />
+          <p className="text-red-500 text-[12px] mt-[-5px]">
+            {errors?.email && errors.email}
+          </p>
           <InputField
             name="contact"
-            type="number"
+            type="text"
             length={"10"}
             value={user.contact}
             handleChange={handleChange}
           />
+          <p className="text-red-500 text-[12px] mt-[-5px]">
+            {errors?.contact && errors.contact}
+          </p>
           <div className="flex justify-between gap-1">
-            <DropDown name="state" data={data.map((state) => state.state)} />
-            <DropDown name="district" data={selectedState?.districts} />
+            <DropDown nameEl="state" data={data.map((state) => state.state)} />
+            <DropDown nameEl="district" data={selectedState?.districts} />
           </div>
           <Captcha />
+          <p className="text-red-500 text-[12px]">
+            {errors?.captcha && errors.captcha}
+          </p>
           <input
             type="submit"
             className="mt-[20px] bg-primary-color text-white py-[8px] rounded-md cursor-pointer hover:bg-primary-dark"
