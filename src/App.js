0d613a811ee6a178b9aadab4d2d6dde537989485
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 12:02:17 +0530

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Form } from "./pages/Form";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App flex justify-center items-center min-h-screen">
-      <Form />
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
