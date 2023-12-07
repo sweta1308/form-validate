10e9e44f326c634fa4b45f4d34d1b4c87bed309e
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 15:52:39 +0530

    feat: add index page

---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+import { nameValidation } from "./NameValidation";
+import { emailValidation } from "./EmailValidation";
+import { contactValidation } from "./ContactValidation";
+import { handleSubmit } from "./HandleSubmit";
+
+export { nameValidation, emailValidation, contactValidation, handleSubmit };
