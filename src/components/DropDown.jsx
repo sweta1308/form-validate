7501c1e25e55de2ce5bf8810ab43122616fcf464
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 12:03:29 +0530

    fix: name prop

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,6 +1,6 @@
 import { useUser } from "../context/UserContext";
 
-export const DropDown = ({ data, name }) => {
+export const DropDown = ({ data, nameEl }) => {
   const { user, setUser } = useUser();
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -10,15 +10,16 @@ export const DropDown = ({ data, name }) => {
   return (
     <div>
       <label className="my-[6px] flex flex-col">
-        <span>{name?.charAt(0)?.toUpperCase() + name?.slice(1)}: </span>
+        <span>{nameEl?.charAt(0)?.toUpperCase() + nameEl?.slice(1)}: </span>
         <select
-          name={name}
-          value={user.name}
+          required
+          name={nameEl}
+          value={user.nameEl}
           onChange={handleChange}
           className="w-[145px] border border-slate-400 outline-none px-[10px] py-[5px] text-[12px] rounded"
         >
           <option value="">
-            Select {name?.charAt(0)?.toUpperCase() + name?.slice(1)}
+            Select {nameEl?.charAt(0)?.toUpperCase() + nameEl?.slice(1)}
           </option>
           {data?.map((item, i) => (
             <option key={i} value={item}>
