05069aa06cf2ab17073a97da5d5fffc04b977150
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 21:41:07 +0530

    remove files

--- src/utils/EmailValidation.js
+++
@@ -1,9 +0,0 @@
-export const emailValidation = (e, setUser, setErrors) => {
-  const { value } = e.target;
-  if (!value) {
-    setErrors((prev) => ({ ...prev, email: "This is a required field." }));
-  } else {
-    setUser((prev) => ({ ...prev, email: value }));
-    setErrors((prev) => ({ ...prev, email: "" }));
-  }
-};
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-import { nameValidation } from "./NameValidation";
-import { emailValidation } from "./EmailValidation";
-import { contactValidation } from "./ContactValidation";
-import { handleSubmit } from "./HandleSubmit";
-
-export { nameValidation, emailValidation, contactValidation, handleSubmit };
