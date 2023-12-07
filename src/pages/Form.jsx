2670ff5cbc8ddc8ca3d53795f4dee1461df4f385
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 15:51:25 +0530

    feat: add validations

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -4,82 +4,60 @@ import { DropDown } from "../components/DropDown";
 import { InputField } from "../components/InputField";
 import { useUser } from "../context/UserContext";
 import { data } from "../data/StateData";
+import {
+  contactValidation,
+  emailValidation,
+  handleSubmit,
+  nameValidation,
+} from "../utils";
+import { Error } from "../components/Error";
 
 export const Form = () => {
   const navigate = useNavigate();
   const { user, setUser, errors, setErrors, captcha } = useUser();
   const selectedState = data.find((state) => state.state === user.state);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
-    setErrors({ ...errors, [name]: "" });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!/^[a-zA-Z]+(?:\s[a-zA-Z]+)?$/.test(user.name)) {
-      setErrors({ ...errors, name: "Name can only contain alphabets." });
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
-      setErrors({ ...errors, email: "Please provide a valid email." });
-    } else if (!/^[0-9]+$/.test(user.contact)) {
-      setErrors({
-        ...errors,
-        contact: "Contact should only contain numbers.",
-      });
-    } else if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
-      setErrors({ ...errors, captcha: "Please provide correct value." });
-    } else {
-      localStorage.setItem("data", JSON.stringify({ user }));
-      navigate("/home");
-    }
-  };
   return (
     <>
       <div className="w-[360px] p-[30px] shadow-lg rounded xs:w-[320px] xs:p-[0px] xs:px-[15px] xs:py-[20px]">
         <h1 className="font-extrabold text-[36px] text-center text-primary-dark mb-[20px]">
           Apply
         </h1>
-        <form className="flex flex-col" onSubmit={handleSubmit}>
+        <Error>{errors.error && errors.error}</Error>
+        <form
+          className="flex flex-col"
+          onSubmit={(e) => handleSubmit(e, user, captcha, setErrors, navigate)}
+        >
           <InputField
             name="name"
             type="text"
             value={user.name}
-            handleChange={handleChange}
+            handleChange={(e) => nameValidation(e, setUser, setErrors)}
           />
-          <p className="text-red-500 text-[12px] mt-[-5px]">
-            {errors?.name && errors.name}
-          </p>
+          <Error>{errors?.name && errors.name}</Error>
           <InputField
             name="email"
             type="text"
             value={user.email}
-            handleChange={handleChange}
+            handleChange={(e) => emailValidation(e, setUser, setErrors)}
           />
-          <p className="text-red-500 text-[12px] mt-[-5px]">
-            {errors?.email && errors.email}
-          </p>
+          <Error>{errors?.email && errors.email}</Error>
           <InputField
             name="contact"
             type="text"
-            length={"10"}
             value={user.contact}
-            handleChange={handleChange}
+            handleChange={(e) => contactValidation(e, setUser, setErrors)}
           />
-          <p className="text-red-500 text-[12px] mt-[-5px]">
-            {errors?.contact && errors.contact}
-          </p>
+          <Error>{errors?.contact && errors.contact}</Error>
           <div className="flex justify-between gap-1">
             <DropDown nameEl="state" data={data.map((state) => state.state)} />
             <DropDown nameEl="district" data={selectedState?.districts} />
           </div>
           <Captcha />
-          <p className="text-red-500 text-[12px]">
-            {errors?.captcha && errors.captcha}
-          </p>
+          <Error>{errors?.captcha && errors.captcha}</Error>
           <input
             type="submit"
-            className="mt-[20px] bg-primary-color text-white py-[8px] rounded-md cursor-pointer hover:bg-primary-dark"
+            className="mt-[30px] bg-primary-color text-white py-[8px] rounded-md cursor-pointer hover:bg-primary-dark"
           />
         </form>
       </div>
