46a709b3e6f567861aeb56e1ddea647105b149bf
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-07 21:41:56 +0530

    refactor: handle submit function

--- src/utils/HandleSubmit.js
+++ src/utils/HandleSubmit.js
@@ -1,17 +1,14 @@
 export const handleSubmit = (e, user, captcha, setErrors, navigate) => {
   e.preventDefault();
-  if (
-    user.name.length === 0 ||
-    user.email.length === 0 ||
-    user.contact.length === 0 ||
-    user.state.length === 0 ||
-    user.district.length === 0 ||
-    user.captcha.length === 0
-  ) {
-    setErrors((prev) => ({ ...prev, error: "All fields are required." }));
-  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.email)) {
-    setErrors((prev) => ({ ...prev, email: "Please provide a valid email." }));
-  } else if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
+  const fields = Object.keys(user).slice(0, 5);
+  const newValidationErrors = fields.reduce((acc, field) => {
+    if (!user[field].trim()) {
+      acc[field] = "This field is required.";
+    }
+    return acc;
+  }, {});
+  setErrors(newValidationErrors);
+  if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
     setErrors((prev) => ({
       ...prev,
       captcha: "Please provide correct value.",
