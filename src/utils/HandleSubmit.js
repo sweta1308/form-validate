540b95207a42f8e7c29c015c8876c4d3667c6480
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-12-08 11:13:56 +0530

    refactor: handle submit function

--- src/utils/HandleSubmit.js
+++ src/utils/HandleSubmit.js
@@ -1,29 +1,31 @@
 import { contactValidation, emailValidation, nameValidation } from "./Validate";
 
-export const handleSubmit = (e, user, captcha, setErrors, navigate) => {
+export const handleSubmit = (e, user, captcha, errors, setErrors, navigate) => {
   e.preventDefault();
   const fields = Object.keys(user).slice(0, 5);
   const newValidationErrors = fields.reduce((acc, field) => {
     if (!user[field].trim()) {
       acc[field] = "This field is required.";
     }
+    if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
+      acc.captcha = "Please provide correct value.";
+    }
     return acc;
   }, {});
   setErrors(newValidationErrors);
-  if (user.name) {
-    nameValidation(user.name, setErrors);
-  }
-  if (user.email) {
-    emailValidation(user.email, setErrors);
-  }
-  if (user.contact) {
-    contactValidation(user.email, setErrors);
-  }
-  if (captcha.captcha1 + captcha.captcha2 !== Number(user.captcha)) {
-    setErrors((prev) => ({
-      ...prev,
-      captcha: "Please provide correct value.",
-    }));
+  if (
+    Object.keys(newValidationErrors).length > 0 ||
+    Object.values(errors).some((item) => item.length > 0)
+  ) {
+    if (user.name) {
+      nameValidation(user.name, setErrors);
+    }
+    if (user.email) {
+      emailValidation(user.email, setErrors);
+    }
+    if (user.contact) {
+      contactValidation(user.contact, setErrors);
+    }
   } else {
     localStorage.setItem("data", JSON.stringify({ user }));
     navigate("/home");
